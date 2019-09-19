f8d371a9983a20a3439f7eea403652c9dfb84608
duyduong65 <tg.bluesky66@gmail.com>
2019-09-19 09:35:24 +0700

    create 5 question

---
+++ Main_App_Ai_La_Trieu_Phu.js
@@ -0,0 +1,28 @@
+// document.getElementById('inputValue').innerHTML = "alo";
+let arrQuestion = [
+    "Mạnh vì..., bạo vì tiền",
+    // A. Gạo -
+    // B. Vàng
+    // C. Lúa
+    // D. Giàu
+    "Cây ngay không sợ chết... ",
+    // A. Cháy
+    // B. Ngạt
+    // C. Gãy
+    // D. Đứng -
+    "Sat trong tiếng Anh là thứ mấy trong tuần",
+    // A. Thứ ba
+    // B. Thứ năm -
+    // C. Chủ nhật
+    // D. Thứ bảy
+    "'Bên trên là ngói, bên dưới là hang là' là cái gì?",
+    // A. Miệng -
+    // B. Tai
+    // C. Đầu gối
+    // D. Bàn chân
+    "Liên đoàn bóng đá Úc thuộc liên đoàn bóng đá nào?"
+    // A. Châu Á -
+    // B. Châu Âu
+    // C. Châu Úc
+    // D. Châu Mỹ
+];
