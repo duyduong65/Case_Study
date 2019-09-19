a2eb46f7a19a8df324ed0c6d1ce481dcd78355f9
duyduong65 <tg.bluesky66@gmail.com>
2019-09-19 17:12:29 +0700

    create class answer and question

--- Main_App_Ai_La_Trieu_Phu.js
+++ Main_App_Ai_La_Trieu_Phu.js
@@ -1,12 +1,3 @@
-document.getElementById('requestAnswer').innerHTML = "Mạnh vì..., bạo vì tiền?";
-document.getElementById('answer_1').value = "A: Gạo";
-document.getElementById('answer_2').innerHTML = "B: Vàng";
-document.getElementById('answer_3').innerHTML = "C: Lúa";
-document.getElementById('answer_4').innerHTML = "D: Đất";
-
-function abc() {
-    alert("kjskdjfhkasd");
-}
 
 let arrQuestion = [
     "Mạnh vì..., bạo vì tiền",
@@ -20,14 +11,13 @@ let arrQuestion = [
     "Ủy ban nhân dân do ai bầu ra?",
     "Vua nào đặt nhiều niên hiệu nhất lịch sử nước ta?",
     "Lần đầu tiên nước ta dùng bộc phá 1000 kg thuốc nổ đánh giặc là ở đâu?",
-    "Đất nước nào là quê hương của ông già tuyết? ",
+    "Đất nước nào là quê hương của ông già tuyết?",
     "Ở Chùa Bộc, ngoài thờ phật, nhân dân còn thờ vị tướng nào?",
     "Phim hoạt hình đầu tiên được công chiếu vào thời gian nào?",
     "Lớp trưởng lớp C0819H2 tên là gì?"
 ];
 let arrAnswer = ["Gạo", "Đứng", "Thứ bảy", "Miệng", "Châu Á", "CaCO3", "Thái Nguyên", "Biển Chết", "Hội đồng nhân dân", "Lý Nhân Tông",
-    "Điện Biên Phủ", "Phần Lan", "Quang Trung", "28-10-1892", "Lê Thanh Hải"
-];
+    "Điện Biên Phủ", "Phần Lan", "Quang Trung", "28-10-1892", "Lê Thanh Hải"];
 let arrAnswer_A = ["Gạo", "Cháy", "Thứ ba", "Miệng", "Châu Á", "CaO", "Bắc Ninh", "Caribean", "Hội đồng nhân dân", "Hồ Quý Ly",
     "Đồi A1", "Phần Lan", "Lý Công Uẩn", "5-10-1992", "An Xuân Bách"];
 let arrAnswer_B = ["Vàng", "Ngạt", "Thứ năm", "Tai", "Châu Âu", "CaOH2", "Thái Nguyên", "Đại Tây Dương", "Hội đồng quản trị xã",
@@ -35,4 +25,84 @@ let arrAnswer_B = ["Vàng", "Ngạt", "Thứ năm", "Tai", "Châu Âu", "CaOH2",
 let arrAnswer_C = ["Lúa", "Gãy", "Chủ nhật", "Đầu gối", "Châu Úc", "CaCO3", "Lào Cai", "Thái Bình Dương", "Hội đồng quản trị huyện",
     "Lý Thái tổ", "Điện Biên Phủ", "Hà Lan", "Nguyễn Huệ", "27-3-1981", "Tùng 10m question"];
 let arrAnswer_D = ["Đất", "Đứng", "Thứ bảy", "Bàn chân", "Châu Mỹ", "C6H12O6", "Tuyên Quang", "Biển Chết", "Hội đồng quản trị tỉnh",
-    "Lý Nhân Tông", "Cửa Khẩu Thanh Thủy", "Bỉ", "Quang Trung", "6-6-1983", "Lê Duy Dương",];
+    "Lý Nhân Tông", "Cửa Khẩu Thanh Thủy", "Bỉ", "Quang Trung", "6-6-1983", "Lê Duy Dương"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let count = 0;
+
+function collectAnswer(){
+
+}
+
+let Answer = function(value,nameButton) {
+    this._value = value;
+
+    this.getValue = function () {
+        return this._value;
+    };
+    this.setAnswer = function () {
+        document.getElementById(nameButton).innerHTML = this.getValue();
+    }
+};
+
+let Question = function(value,nameButton){
+    this._value = value;
+
+    this.getValue = function () {
+        return this._value;
+    };
+    this.setQuestion = function () {
+        document.getElementById(nameButton).innerHTML = this.getValue();
+    }
+};
+
+let value_question = arrQuestion[count];
+let value_answer_A = arrAnswer_A[count];
+let value_answer_B = arrAnswer_B[count];
+let value_answer_C = arrAnswer_C[count];
+let value_answer_D = arrAnswer_D[count];
+
+let question = new Question(value_question,"'question'");
+let answer_A = new Answer(value_answer_A,"'answer_A'");
+let answer_B = new Answer(value_answer_B,"'answer_B'");
+let answer_C = new Answer(value_answer_C,"'answer_C'");
+let answer_D = new Answer(value_answer_D,"'answer_D'");
+
+function display(){
+    document.getElementById("question").innerHTML = question.getValue();
+    document.getElementById("answer_A").innerHTML = answer_A.getValue();
+    document.getElementById("answer_B").innerHTML = answer_B.getValue();
+    document.getElementById("answer_C").innerHTML = answer_C.getValue();
+    document.getElementById("answer_D").innerHTML = answer_D.getValue();
+}
